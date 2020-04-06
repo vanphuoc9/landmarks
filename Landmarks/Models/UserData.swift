4f7dc4f640cbaed399106683950dc8c548b6a2da
vanphuoc9 <thaiphuoc1997@gmail.com>
2020-04-06 22:12:47 +0700

    Handling User Input

---
+++ Landmarks/Models/UserData.swift
@@ -0,0 +1,17 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by tvphuoc on 4/6/20.
+//  Copyright © 2020 tvphuoc. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoriteOnly = false
+    @Published var landmarks = landmarkData
+    
+    
+}
