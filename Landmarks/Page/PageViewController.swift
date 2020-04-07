a875fe54f29ad6420d63979b2289b8af2f6d0104
vanphuoc9 <thaiphuoc1997@gmail.com>
2020-04-07 15:36:06 +0700

    Interfacing with UIKit

--- Landmarks/Page/PageViewController.swift
+++ Landmarks/Page/PageViewController.swift
@@ -10,18 +10,74 @@ import UIKit
 import SwiftUI
 
 struct PageViewController: UIViewControllerRepresentable {
+    
     var controllers: [UIViewController]
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
     
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
 
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
            pageViewController.setViewControllers(
-               [controllers[0]], direction: .forward, animated: true)
+               [controllers[currentPage]], direction: .forward, animated: true)
        }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource,
+        UIPageViewControllerDelegate{
+        var parent: PageViewController
+        
+        init(_ pageViewController: PageViewController){
+            self.parent = pageViewController
+        }
+        
+        // Page before
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index == 0 {
+                return parent.controllers.last
+            }
+
+            return parent.controllers[index - 1]
+        }
+        
+        // Page after
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            
+            return parent.controllers[index + 1]
+        }
+        
+        //SwiftUI calls this method whenever a page switching animation completes, you can find the index of the current view controller and update the binding.
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            
+            if completed, let visibleViewController = pageViewController.viewControllers?.first,
+                let index = parent.controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
+            
+        }
+    }
+
 }
